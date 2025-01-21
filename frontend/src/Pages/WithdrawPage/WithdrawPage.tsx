a914ebda01393640a8a9eb46c9944bf25ce3c28b
kishore2812 <kishore9743kumar@gmail.com>
2025-01-21 11:18:42 +0530

    modified:   frontend/src/Pages/WithdrawPage/WithdrawPage.tsx 	modified:   frontend/src/Pages/WithdrawPage/withdrawPage.scss

--- frontend/src/Pages/WithdrawPage/WithdrawPage.tsx
+++ frontend/src/Pages/WithdrawPage/WithdrawPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./withdrawPage.scss";
 import Header from "../../Components/Header/Header";
+import { CiCirclePlus } from "react-icons/ci";
 
 const Withdraw = () => {
   const [amount, setAmount] = useState("");
@@ -15,31 +16,38 @@ const Withdraw = () => {
         <div className="withdraw-page-card">
           <div className="withdraw-page-card-section wallet-balance">
             <span>Wallet Balance</span>
-            <span>$1000</span>
+            <span>₹14587</span>
           </div>
           <div className="withdraw-page-card-section smaller-text">
             <span>Total Withdrawal Amount</span>
-            <span>$500</span>
+            <span>₹14000</span>
           </div>
           <div className="withdraw-page-card-section smaller-text">
             <span>Incentive Claims</span>
-            <span>$100</span>
+            <span>₹587</span>
           </div>
           <div className="withdraw-page-input-section">
-            <label htmlFor="withdrawAmount">Enter Amount to Withdraw</label>
-            <input
-              type="number"
-              id="withdrawAmount"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              placeholder="Withdraw Amount"
-            />
+            <div className="input-withdraw-container">
+              <input
+                type="number"
+                id="withdrawAmount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter The amount"
+              />
+              <button type="button" className="withdraw-button">
+                Withdraw
+              </button>
+            </div>
           </div>
         </div>
 
         {/* Second Card */}
         <div className="withdraw-page-card">
-          <div className="withdraw-page-credit-card-placeholder"></div>
+          <div className="withdraw-page-credit-card-placeholder">
+            <CiCirclePlus className="plus-icon" />
+            <p>Add Credit Card</p>
+          </div>
         </div>
       </div>
     </div>
